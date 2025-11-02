8c554bcffaf1c6f3c52a25dd7be34bac1d5cb526
Nelson Damico Junior <neodamico@gmail.com>
2025-11-02 00:24:42 -0300

    Update inicial dos arquivos do projeto

--- src/main/java/com/murdle/project/ProjectMurdleApplication.java
+++ src/main/java/com/murdle/project/ProjectMurdleApplication.java
@@ -1,13 +1,22 @@
 package com.murdle.project;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectMurdleApplication {
+    public static void main(String[] args) {
+        // Carrega .env (se existir)
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectMurdleApplication.class, args);
-	}
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
 
+        SpringApplication.run(ProjectMurdleApplication.class, args);
+    }
 }
