8b3c4d16be7e4013a814695ee58faeba1a4153b6
Nelson Damico Junior <neodamico@gmail.com>
2025-11-02 01:26:02 -0300

    alteração de arquivos para funcionamento do render

--- src/main/java/com/murdle/project/ProjectMurdleApplication.java
+++ src/main/java/com/murdle/project/ProjectMurdleApplication.java
@@ -4,19 +4,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// src/main/java/com/murdle/project/ProjectMurdleApplication.java
 @SpringBootApplication
 public class ProjectMurdleApplication {
     public static void main(String[] args) {
-        // Carrega .env (se existir)
-        Dotenv dotenv = Dotenv.configure()
-                .directory("./")
-                .ignoreIfMissing()
-                .load();
-
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
-        );
-
         SpringApplication.run(ProjectMurdleApplication.class, args);
     }
 }
